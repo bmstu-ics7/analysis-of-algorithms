9b3f0a01686276d1050006d2c7cbf63030da9d74
Willsem <happymainer132@yandex.ru>
2019-10-22 02:48:19 +0300

    FINISH: lab_04

---
+++ lab_04/source/ThreadMultiplication.cc
@@ -0,0 +1,40 @@
+#include "ThreadMultiplication.h"
+
+void ThreadMultiplication::multiplication(const Matrix& first, const Matrix& second,
+                                          Matrix& result,
+                                          const int start, const int finish)
+{
+    const int m = first.size();
+    const int n = second.size();
+    const int q = second[0].size();
+
+    Array mulU = CreateArray(m);
+    Array mulV = CreateArray(q);
+
+    for (int i = start; i <= finish; ++i)
+        for (int j = 0; j < n - 1; j += 2)
+            mulU[i] -=
+                first[i][j] * first[i][j + 1];
+
+    for (int j = 0; j < q; ++j)
+        for (int i = 0; i < n - 1; i += 2)
+            mulV[j] -=
+                second[i][j] * second[i + 1][j];
+
+    for (int i = start; i <= finish; ++i) {
+        for (int j = 0; j < q; ++j) {
+            result[i][j] = mulU[i] + mulV[j];
+            for (int k = 0; k < n - 1; k += 2) {
+                result[i][j] +=
+                    (first[i][k] + second[k + 1][j]) *
+                    (first[i][k + 1] + second[k][j]);
+            }
+        }
+    }
+
+    if (n % 2 == 1)
+        for (int i = start; i <= finish; ++i)
+            for (int j = 0; j < q; ++j)
+                result[i][j] +=
+                    first[i][n - 1] * second[n - 1][j];
+}
