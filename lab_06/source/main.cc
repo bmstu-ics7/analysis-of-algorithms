35807d4a0c730442b7bfddf38af5a0e7b813eaf2
Willsem <happymainer132@yandex.ru>
2019-12-19 11:22:28 +0300

    [EDT] beauty main

--- lab_06/source/main.cc
+++ lab_06/source/main.cc
@@ -14,15 +14,17 @@ int main(int argc, char* argv[])
 
     auto result = FullSearch::way(matrix);
 
+    std::cout << BOLD_ON << "Алгоритм перебора:" << BOLD_OFF << std::endl;
     for (int i = 0; i < result.first.size(); ++i)
         std::cout << result.first[i] << ' ';
     std::cout << std::endl;
 
-    std::cout << "dist = " << result.second << std::endl;
+    std::cout << "dist = " << result.second << std::endl << std::endl;
 
     // alpha, beta, rho, tmax
     result = AntSearch::way(matrix, 0.2, 0.2, 0.8, 200);
 
+    std::cout << BOLD_ON << "Муравьиный алгоритм:" << BOLD_OFF << std::endl;
     for (int i = 0; i < result.first.size(); ++i)
         std::cout << result.first[i] << ' ';
     std::cout << std::endl;
